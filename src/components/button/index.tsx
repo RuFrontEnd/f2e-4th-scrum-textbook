599ff7c2074d9bb7777a3fad8ec0c02416ba46cd
Luke <luke_xie@wistron.com>
2023-04-11 13:18:21 +0800

    修改 button 字體

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,7 +4,7 @@ import type * as Types from "types";
 function Button(props: Types.Components.Button.Props) {
   return (
     <button
-      className={`ff-roboto ${styles.button} ${props.className} ${
+      className={`ff-roboto fw-bold ${styles.button} ${props.className} ${
         props.disabled && styles.disabled
       }`}
       style={{ width: props.width }}
